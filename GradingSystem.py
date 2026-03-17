7febae3d16f28c6e4d2bae1d8ec4fc4a75f33f88
Prajakta Roy Arpa <prajaktaroyarpa05@gmail.com>
2026-03-18 00:52:31 +0800

    Clean project structure

--- GradingSystem.py
+++
@@ -1,8 +0,0 @@
-def GradingSystem(score):
-    if ( score%5>=3 and score>=38 and n<=90):
-        return score+(5-(score%5))
-    else:
-        print(score)
-    
-    
-     
--- models.py
+++ models.py
@@ -1,17 +1,4 @@
 print("Incident class started")
 
-class Incident:
-    def __init__(self,incidentid,title,attacktype,severity,status): 
-        self.incidentid = incidentid
-        self.title = title
-        self.attacktype = attacktype
-        self.severity = severity
-        self.status = status
 
-    def display(self):
-        print(f"Incident ID: {self.incidentid}")
-        print(f"Title: {self.title}")
-        print(f"Attack Type: {self.attacktype}")
-        print(f"Severity: {self.severity}")
-        print(f"Status: {self.status}")
-        
+
