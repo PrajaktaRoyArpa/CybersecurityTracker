1f6b24eddbeca76aa862afde914a32cb2e92d62b
Prajakta Roy Arpa <prajaktaroyarpa05@gmail.com>
2026-03-18 08:21:01 +0800

    Added list and default staus in Incident manager

--- manager.py
+++ manager.py
@@ -1,12 +1,13 @@
-from models import Incident
-
+from models import Incident 
 class IncidentManager: 
     def __init__(self): 
-        self.incidets = []
-        self._next_id = 1
-
-    def _generate_id(self): 
-        incident_id = self._next_id
-        self._next_id += 1
-        return incident_id
+        self.incidents = []
+        self.next_id = 1
+    
+    def create_incident(self, title, attack_type, severity, status):
+        status = "Open"
+        incident = Incident(self.next_id, title, attack_type, severity, status)
+        self.incidents.append(incident)
+        self.next_id += 1
+        return incident
     
