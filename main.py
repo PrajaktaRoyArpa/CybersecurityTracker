cd63953bdba5c6863d667c0bf7362abe73372a1d
Prajakta Roy Arpa <prajaktaroyarpa05@gmail.com>
2026-03-19 22:45:43 +0800

    Added Delete and Search functions.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 from manager import IncidentManager
 
 def main():
@@ -7,8 +8,10 @@ def main():
         print("Cybersecurity Incident Tracker")
         print("1. Create Incident")
         print("2. List Incidents")
-        print("3. Exit")
-        
+        print("3. Update Incident status")
+        print("4. Delete Incident")
+        print("5. Search Incidents")
+        print("6. Exit")        
         choice = input("Enter your choice: ")
 
         if choice == "1":
@@ -21,7 +24,41 @@ def main():
         elif choice == "2":
             manager.list_incidents()
         
-        elif choice == "3":
+        elif choice == "3": 
+            try: 
+                incident_id = int (input("Enter incident ID: "))
+                new_status = input("Enter new status (open/resolved/closed): ")
+                success = manager.update_status(incident_id, new_status)
+                if success: 
+                    print("Status updated successfully!")
+                else:
+                    print("Incident not found.")
+
+            except ValueError:
+                print("Invalid input. Please enter a valid incident ID.")
+        
+        elif choice == "4":
+            try:
+                incident_id = int(input("Enter incident ID to delete: "))
+                success = manager.delete_incident(incident_id)
+                if success:
+                    print("Incident deleted successfully!")
+                else:
+                    print("Incident not found.")
+            except ValueError:
+                print("Invalid input. Please enter a valid incident ID.")
+        
+        elif choice == "5":
+            keyword = input("Enter keyword to search or leave blank to skip: ")
+            severity = input("Enter severity to filter (Low/Medium/High/leave blank to skip): ")
+            status = input("Enter status to filter (open/resolved/closed/leave blank to skip): ")
+            results = manager.search_incidents(keyword if keyword else None, severity if severity else None)
+            if results:
+                manager.display_custom_list(results)
+            else:
+                print("No matching incidents found.")
+        
+        elif choice == "6":
             print("Exiting...")
             break
         
